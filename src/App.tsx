11bf73e0897b1df02de357ca939a01995e61da3b
Darren Tham <darrent9859@gmail.com>
2022-12-20 01:53:34 -0500

    Focus on input after animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,62 @@
-const ANIMATION_DURATION = 750
+import { useState, useEffect, useRef } from 'react'
+
+const ANIMATION_DURATION = 1000
+const ANIMATION_DELAY = 750
+const ELEM_NUM = 3
 
 function App() {
-  return (
-    <div className='prompt'>
-        <span style={{ animationDelay: formatDelay(1) }}>Type a Word</span>
-        <input type='text' style={{ animationDelay: formatDelay(2) }}></input>
-        <button className='elementify' style={{ animationDelay: formatDelay(3) }}>Elementify</button>
-    </div>
-  )
+    const [styles, setStyles] = useState(initStyles(ELEM_NUM))
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setStyles(new Array(3).fill({ opacity: 1 }))
+            inputRef.current?.focus()
+        }, ANIMATION_DURATION + ANIMATION_DELAY * ELEM_NUM)
+    }, [])
+
+    return (
+        <div className='prompt'>
+            <span style={styles[0]}>Type a Word</span>
+            <input
+                type='text'
+                style={styles[1]}
+                ref={inputRef}
+            />
+            <button
+                className='elementify'
+                style={styles[2]}
+                onClick={e => handleClick(e, setStyles)}
+            >Elementify
+            </button>
+        </div>
+    )
 }
 
-function formatDelay(n: number) {
-    return `${ANIMATION_DURATION * n}ms`
+function initStyles(elemNum: number) {
+    const styles: React.CSSProperties[] = []
+    let delay = ANIMATION_DELAY
+    for (let i = 0; i < elemNum; i++) {
+        styles.push({
+            opacity: 0,
+            pointerEvents: 'none',
+            animationName: 'appear',
+            animationDuration: `${ANIMATION_DURATION}ms`,
+            animationDelay: `${delay}ms`,
+            animationFillMode: 'forwards'
+        })
+        delay += ANIMATION_DELAY
+    }
+    return styles
+}
+
+function handleClick(e: React.MouseEvent, setStyles: React.Dispatch<React.SetStateAction<React.CSSProperties[]>>) {
+    const styles: React.CSSProperties[] = []
+    for (let i = 0; i < 3; i++) {
+        styles.push({ animationFillMode: 'none' })
+    } 
+    setStyles(styles)
+
 }
 
 export default App
