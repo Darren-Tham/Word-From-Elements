c9b43b5814589fbec28ceecb753d923aee6ae50f
Darren Tham <darrent9859@gmail.com>
2022-12-19 23:44:52 -0500

    Add animation to prompt

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+const ANIMATION_DURATION = 750
+
 function App() {
   return (
-    <div>Choose a word</div>
+    <div className='prompt'>
+        <span style={{ animationDelay: formatDelay(1) }}>Type a Word</span>
+        <input type='text' style={{ animationDelay: formatDelay(2) }}></input>
+        <button className='elementify' style={{ animationDelay: formatDelay(3) }}>Elementify</button>
+    </div>
   )
+}
+
+function formatDelay(n: number) {
+    return `${ANIMATION_DURATION * n}ms`
 }
 
 export default App
